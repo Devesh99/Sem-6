67fb71ed6e9c45389554ddfe70d5d909c89d94b2
Devesh Varma <51313129+Devesh99@users.noreply.github.com>
2020-02-01 19:50:41 +0530

    Create Exp-2.cpp

---
+++ CSS/Exp-2.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+using namespace std;
+
+#define deb(x) cout << #x << " " << x << endl;
+#define For(i,n) for(int i=0;i<n;i++)
+
+int main()
+{
+    string key,pt,ct;
+    int t,i,j,k,lenk,lenm,row;
+
+    cout<<"Enter a key: ";
+    cin>>key;
+    lenk=key.length();
+    int keyarr[lenk];
+    For(i,lenk){
+        keyarr[i]=key[i]-97;
+    }
+    cout<<"Enter Message to be encrypted: ";
+    cin>>pt;
+    lenm=pt.length();
+    For(i,lenm){
+        ct.push_back((pt[i]-97+keyarr[i%lenk])%26+97);
+    }
+    cout<<"The message after Substitution Cipher is: "<< ct<<endl;
+    
+    row=(lenm+4)/5;
+    int tr[row][5];
+    k=0;
+    For(i,row){
+        For(j,5){
+            if(ct[k]=='\0')
+                tr[i][j]='_';
+            else
+                tr[i][j]=ct[k++];
+        }
+    }
+    cout<<"Transpose Matrix:\n";
+    For(i,row){
+        For(j,5){
+            printf("%c ",tr[i][j]);
+        }
+        cout<<'\n';
+    }
+    
+    cout<<"Message after Transposition Cipher is: ";
+    For(i,5){
+        For(j,row){
+            printf("%c",tr[j][i]);
+        }
+    }
+    cout<<"\nDecrypted Message is: ";
+    k=0;
+    For(i,row){
+        For(j,5){
+            if(tr[i][j]=='_')
+                continue;
+            else{
+                pt[k]=(tr[i][j]-97-keyarr[k%lenk]+26)%26+97;
+                ++k;
+            }
+        }
+    }
+    cout<<pt;
+    
+    return 0;
+}
