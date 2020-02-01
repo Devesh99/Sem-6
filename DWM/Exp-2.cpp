f43fde6c7bb49bd7a6b0df0167e8599bee027984
Devesh Varma <51313129+Devesh99@users.noreply.github.com>
2020-02-01 21:20:47 +0530

    Improved Algo

--- DWM/Exp-2.cpp
+++ DWM/Exp-2.cpp
@@ -2,7 +2,6 @@
 #include <cmath>
 using namespace std;
 
-
 float dist(float x,float y,float x1,float y1){
     float mx=(x-x1)*(x-x1);
     float my=(y-y1)*(y-y1);
@@ -19,12 +18,11 @@ int main()
     float p1[x][2];
     float p2[x][2];
     //{{1.0,1.0},{1.5,2.0},{3.0,4.0},{5.0,7.0},{3.5,5.0},{4.5,5.0},{3.5,4.5}};
-    cout<<"\nEnter X and Y vlaue of each data point:\n ";
+    cout<<"\nEnter X and Y vlaue of each data point:\n";
     for(i=0;i<x;++i){
         cin>>a[i][0]>>a[i][1];
     }
-    
-    
+
     int pp1=-1;
     int pp2=-1;
 
@@ -37,9 +35,50 @@ int main()
     float n1y=0.0;
     float n2x=0.0;
     float n2y=0.0;
-    //while((m1x-n1x)<=0.01&&(m1y-n1y)<=0.01&&(m2x-n2x)<=0.01&&(m2y-n2y)<=0.01){
-    int n=20;
-       while(n>=0){
+    
+    pp1=-1;
+    pp2=-1;
+    dist1=dist(m1x,m1y,a[i][0],a[i][1]);
+    dist2=dist(m2x,m2y,a[i][0],a[i][1]);
+    for(i=0;i<x;++i){
+        dist1=dist(m1x,m1y,a[i][0],a[i][1]);
+        dist2=dist(m2x,m2y,a[i][0],a[i][1]);
+        if(dist1-dist2<=0){
+            pp1++;
+            p1[pp1][0]=a[i][0];
+            p1[pp1][1]=a[i][1];
+        }
+        else{
+            pp2++;
+            p2[pp2][0]=a[i][0];
+            p2[pp2][1]=a[i][1];
+        }
+    }
+    dist1=0.0;
+    dist2=0.0;
+    for(i=0;i<=pp1;++i){
+        dist1+=p1[i][0];
+        dist2+=p1[i][1];
+    }
+    n1x=dist1/(pp1+1);
+    n1y=dist2/(pp1+1);
+
+    dist1=0.0;
+    dist2=0.0;
+    for(i=0;i<=pp2;++i){
+        dist1+=p2[i][0];
+        dist2+=p2[i][1];
+    }
+   // cout<<"it->"<<21-n<<endl;
+    n2x=dist1/(pp2+1);
+    n2y=dist2/(pp2+1);
+    
+    
+    int n=0;
+    while((m1x-n1x)>=0.01||(m1y-n1y)>=0.01||(m2x-n2x)>=0.01||(m2y-n2y)>=0.01){
+        n++;
+        printf("XX%f %f %f %f\n",m1x,n1x,m1y,n1y);
+        printf("YY%f %f %f %f\n",m2x,n2x,m2y,n2y);
         pp1=-1;
         pp2=-1;
         for(i=0;i<x;++i){
@@ -62,6 +101,8 @@ int main()
             dist1+=p1[i][0];
             dist2+=p1[i][1];
         }
+        m1x=n1x;
+        m1y=n1y;
         n1x=dist1/(pp1+1);
         n1y=dist2/(pp1+1);
  
@@ -71,7 +112,9 @@ int main()
             dist1+=p2[i][0];
             dist2+=p2[i][1];
         }
-        cout<<"it->"<<21-n<<endl;
+        cout<<"iteration ->"<<21-n<<endl;
+        m2x=n2x;
+        m2y=n2y;
         n2x=dist1/(pp2+1);
         n2y=dist2/(pp2+1);
         cout<<m1x<<" "<<m1y<<endl;
@@ -79,7 +122,6 @@ int main()
         cout<<endl;
         cout<<n1x<<" "<<n1y<<endl;
         cout<<n2x<<" "<<n2y<<endl;
-       n--;
         cout<<endl;
         cout<<"m"<<m1x<<" "<<m1y<<" "<<m2x<<" "<<m2y<<endl;
         cout<<"n"<<n1x<<" "<<n1y<<" "<<n2x<<" "<<n2y<<endl;
@@ -93,6 +135,6 @@ int main()
     for(i=0;i<=pp2;++i){
         cout<<p2[i][0]<<" "<<p2[i][1]<<endl;
     }
-    cout<<"Mean  of First cluster "<<"x: "<<n2x<<" y: "<<n2y;
+    cout<<"Mean  of Second cluster "<<"x: "<<n2x<<" y: "<<n2y;
     return 0;
 }
